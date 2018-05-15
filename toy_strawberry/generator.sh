9c634342d633a5ef94a5507fae45b337a3d74a93
lauzingaretti <m.lau.zingaretti@gmail.com>
2018-05-15 16:34:30 +0200

    Update generator.sh

--- toy_strawberry/generator.sh
+++ toy_strawberry/generator.sh
@@ -1,6 +1,2 @@
-Nsample=150
-#shuf Data.chip | head -n $NQTL | sort -gk1,1 -gk2,2 > Y.qtn
-shuf /home/useradmin/Desktop/20_DE_FEBRERO_2017/Aditivos_100/test.gen | head -n $Nsample | sort -gk1,1 -gk2,2 > toy_st.gen
-awk -F'\t' '{print $1" "$2}' 'toy_st.gen' > Data_st.chip  
 NQTL=40
 shuf Data_st.chip | head -n $NQTL | sort -gk1,1 -gk2,2 > Y_st.qtn
