60a161e277b0cd47a5a7c5e11d465bee861a867b
lauzingaretti <m.lau.zingaretti@gmail.com>
2018-09-25 09:32:51 +0200

    Add files via upload

--- toy_strawberry/generator.sh
+++ toy_strawberry/generator.sh
@@ -1,2 +1,10 @@
+Nsample=1500
+#shuf Data.chip | head -n $NQTL | sort -gk1,1 -gk2,2 > Y.qtn
+shuf /home/useradmin/Desktop/20_DE_FEBRERO_2017/Aditivos_100/test.gen | head -n $Nsample | sort -gk1,1 -gk2,2 > toy_st.gen
+awk -F'\t' '{print $1" "$2}' 'toy_st.gen' > Data_st.chip  
 NQTL=40
 shuf Data_st.chip | head -n $NQTL | sort -gk1,1 -gk2,2 > Y_st.qtn
+NQTL1=80
+shuf Data_st.chip | head -n $NQTL1 | sort -gk1,1 -gk2,2 > Y_80.qtn
+NQTL2=150
+shuf Data_st.chip | head -n $NQTL1 | sort -gk1,1 -gk2,2 > Y_150.qtn
--- toy_strawberry/run_toy_st.sh
+++ toy_strawberry/run_toy_st.sh
@@ -1,15 +1,76 @@
 ###run polyploid toy example and sorting files in a new folder.    
  #cat  toy.gen | path-to sbvb -i toy.par
-  cat toy_st.gen | sbvb -i file.par
-           mkdir -p results
-           cp Y.grm.1  results
-           cp Y.outy   results
-           cp Y.outq   results
+ # run the option h2=0.3  0.4 and .0.5 G mimic diploid
+ 
+cat toy_st.gen | ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb -i file_1.par
+           mkdir -p results_example1
+           cp Y.grm.1  results_example1
+           cp Y.outy   results_example1
+           cp Y.outq   results_example1
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy
+        
+        
+#h2= 0.4 y G mimic diploid
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb   -i file_2.par
+           mkdir -p results_example2
+           cp Y.grm.1  results_example2
+           cp Y.outy   results_example2
+           cp Y.outq   results_example2
            rm Y.grm.1 
            rm Y.outq
            rm Y.outy
          
-###done
+         
+         
+#h2= 0.5 y G mimic diploid
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb  -i file_3.par
+           mkdir -p results_example3
+           cp Y.grm.1  results_example3
+           cp Y.outy   results_example3
+           cp Y.outq   results_example3
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy
+         
+#h2 0.5 y G total 
 
-
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb  -i file_4.par
+           mkdir -p results_example4
+           cp Y.grm.1  results_example4
+           cp Y.outy  results_example4
+           cp Y.outq   results_example4
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy
  
+
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb  -i file_5.par
+           mkdir -p results_example5
+           cp Y.grm.1  results_example5
+           cp Y.outy   results_example5
+           cp Y.outq   results_example5
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy
+ 
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb  -i file_6.par
+           mkdir -p results_example6
+           cp Y.grm.1  results_example6
+           cp Y.outy   results_example6
+           cp Y.outq   results_example6
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy
+           
+ cat toy_st.gen |  ~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/src/sbvb  -i file_7.par
+           mkdir -p results_example7
+           cp Y.grm.1  results_example7
+           cp Y.outy   results_example7
+           cp Y.outq   results_example7
+           rm Y.grm.1 
+           rm Y.outq
+           rm Y.outy           
+           
+           
