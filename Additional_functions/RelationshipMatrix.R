8a30a15f4d11339212ba34f669954e76057e5400
lauzingaretti <m.lau.zingaretti@gmail.com>
2018-09-25 09:37:05 +0200

    Add files via upload

---
+++ Additional_functions/RelationshipMatrix.R
@@ -0,0 +1,98 @@
+#generate relationship matrix (see to incorporate ploidy levels, because there are some papers which developed a computation to incorporate ploidy level into relationship matrix)
+
+#'Funtion \code{RelMatrix} 
+#' Creates a additive relationship matrix A from a pedigree data in a 3-column way format based on ploidy level (an even number) and, if ploidy equals 4, based on proportion of parental gametes that are IBD (Identical by Descent) due to double reduction. Returns a dominance relationship matrix if dominance true (ploidy 2 only). Autopolyploid matrices based on Kerr (2012). Construction is based on the Henderson's recursive method described in Mrode (2005).
+#'
+#' @param data pedigree data with 3 column.Unknown are equal to zero. The first column is the individual index, the second the father and the last, the mather 
+#' @param dominance if true, returns the dominance relationship matrix
+#' @param path path to save the file. Default is NULL
+#' #####param slater if true, returns the additive autotetraploid relationship matrix as Slater (2013)
+#' 
+#' @return Matrix with the Relationship between the individuals.
+#' @author M L Zingaretti 
+#'
+#' @references \emph{Chapter 2: Genetic Covariance Between Relatives and Chapter 9: Non-additive Animal Models in Mrode, R. A., and Thompson, R. Linear models for the prediction of animal breeding values. Cabi, 2005.}
+#' @references \emph{Slater, A. T., Wilson, G. M., Cogan, N. O., Forster, J. W., & Hayes, B. J. (2013). Improving the analysis of low heritability complex traits for enhanced genetic gain in potato. Theoretical and Applied Genetics, 1-12.}
+#' @references \emph{Kerr, Richard J., et al. "Use of the numerator relationship matrix in genetic analysis of autopolyploid species." Theoretical and Applied Genetics 124.7 (2012): 1271-1282.}
+#' 
+
+
+RelMatrix <- function(data = NULL, dominance=FALSE,path=NULL){
+  if( is.null(data))
+    stop("The data argument can't be empty") 
+  
+  s <- data[,2]
+  d <- data[,3]
+  
+  if( is.null(s) || is.null(d) )
+    stop(deparse("Please define a validate pedigree file"))
+  
+  if( !is.numeric(s) || !is.numeric(d) )
+    stop(deparse("Pleasy verify your dataset because the pedigree must be numeric"))
+  
+  n <- length(s)
+  A <- matrix(NA,ncol=n,nrow=n)
+  diag(A)<-1
+
+  Time = proc.time()
+
+
+    for(j in 1:(n-1)){
+        for (i in (j+1):(n)){
+        #father and mother unknown 
+        if(s[i] == 0 && d[i] == 0 ){  
+        A[j,i]<-A[i,j]<-0
+        } ## father is unknown
+        if(s[i] == 0 && d[i] != 0 ){
+        A[j,i]<-A[i,j] <- 0.5*(A[j,d[i]])
+        } ##mother is unknown 
+        if(d[i] == 0 && s[i] != 0 ){
+          A[j,i]<-A[i,j] <- 0.5*(A[j,s[i]])
+        }# both are knowing
+        if (d[i] != 0 && s[i] != 0){
+          A[i,i] <- 1+0.5*(A[d[i],s[i]])
+          A[j,i]<-A[i,j] <- 0.5*(A[j,s[i]]+A[j,d[i]])
+        }
+      }
+      }
+ 
+ 
+  if(dominance){
+    cat("Constructing dominance relationship matrix \n")
+    DR <- matrix(NA,ncol=n,nrow=n)
+    diag(DR)<-1
+    for(j in 1:(n-1)){
+      for (i in (j+1):(n)){
+        d1 <- ifelse(length(A[s[i],s[j]])>0,A[s[i],s[j]],0)
+        d2 <- ifelse(length(A[d[i],d[j]])>0,A[d[i],d[j]],0)
+        d3 <- ifelse(length(A[s[i],d[j]])>0,A[s[i],d[j]],0)
+        d4 <- ifelse(length(A[s[j],d[i]])>0,A[s[j],d[i]],0)
+        DR[i,j] <- 0.25*(d1*d2+d3*d4)
+      }
+    }
+    A<-DR
+    DR<-c()
+    }
+  
+  
+  Time = as.matrix(proc.time()-Time)
+  cat("Completed! Time =", Time[3]/60," minutes \n")
+  #      "Visualization options: (matrix, w) \n ")
+  
+  rownames(A) <- colnames(A) <- data[,1]
+  
+  if(!is.null(path)){
+  write.table(A,paste0(path,"/Relationship.mat"),col.names = FALSE,row.names = FALSE,
+              quote=FALSE)
+  }else{
+    write.table(A,"Relationship.mat",sep="\t",col.names = FALSE, row.names=FALSE, 
+                quote=FALSE)
+    
+  }
+  return(A)   
+}
+
+data<-read.table("~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/reversinn1/pSBVB/toy_strawberry/Additional_functions/File_st.ped",header=FALSE)
+#check the dimensions of dataset 
+dim(data)
+RelMatrix(data,dominance=FALSE,path= "~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/reversinn1/pSBVB/toy_strawberry/Additional_functions/")
--- Additional_functions/pedigree.R
+++ Additional_functions/pedigree.R
@@ -1,1 +1,113 @@
+#generate pedigree function 
+#id id_father id_mother [sex] (optional)
+#sex is 1 to males and 2 to females 
+#this function could be created a ped file with any founders, optional sex
+#father 
+#'Funtion \code{PedGenerator} 
+#'This function is use to make a Pedigree file 
+#'@param nfounder number of founders individuals for the population  
+#'@param ngen number of generations into population 
+#'@param nind_bygen vector of ngen lenght indicating the number of individuals into 
+#'each generation 
+#'@param sex logical. If is TRUE, it must be generated a column indicating individual's sex. Default is FALSE 
+#'@param path path to save the pedigree file
+#'@param exlude Must some individuals excludes of analysis?  how many of founder must be excluded of analysis, default is 0. 
+#'@return
+#'  Data.frame with the simulated pedigree. 
+#'  \item{file.ped}{Pedigree file }
+#'
+#'
+#'
+#'@author M L Zingaretti
+#'
+#'
 
+
+pedgenerator<-function(nfounder=10, ngen=5, nind_bygen=c(2,2,3,2,5),sex=FALSE,path=NULL, exclude =0){
+if(nfounder <= 1){
+  stop("nfounder must be a positive integer > 1")
+}
+if(as.integer(nfounder)!=nfounder){
+  stop("nfounder must be a positive integer > 1")
+}
+  
+if(exclude<0){
+  warning("exclude must be a positive value, indicating how many observations are excluded of pedigree")
+exclude=0 
+  }  
+if(ngen<1){
+  stop("the number of generations have to be al least 1")
+}
+
+if(length(nind_bygen)!=ngen){
+ stop("you must be to specify how many individuals have each generation")
+}
+
+if(sex==FALSE){
+            
+init<-data.frame(seq(1,nfounder),rep(0,nfounder),rep(0,nfounder))
+colnames(init)<-c("index","father","mother")
+m= nrow(init)
+deb<-c()
+for(i in 1: ngen){
+deb<-rbind(deb,data.frame(seq(m+1,m+nind_bygen[i]),
+      matrix(unlist(lapply(seq(1:nind_bygen[i]),function(i){sample((exclude+1):m,2)})),ncol=2,byrow=TRUE)))
+m=m+nind_bygen[i]
+  
+}
+colnames(deb)<-c("index","father","mother")
+pedigree<-rbind(init,deb)
+colnames(pedigree)<-c()
+}
+  if(sex==TRUE){
+    
+    
+    deb<-data.frame(seq(1,nfounder),rep(0,nfounder),rep(0,nfounder),unlist(lapply(c(1:nfounder),function(i){sample(c(1,2),1)})))
+    m= nrow(deb)
+    colnames(deb)<-c("index","father","mother","sex")
+    if(exclude>0){
+      a<-c(1:exclude)
+     
+    for(i in 1: ngen){
+      
+      Aux=data.frame(seq(m+1,m+nind_bygen[i]),sample(x=unlist(deb[deb[-a,4]==1,1]),size=nind_bygen[i],replace=TRUE),
+                   sample(unlist(deb[deb[-a,4]==2,1]),nind_bygen[i],replace=TRUE),
+                   unlist(lapply(c(1:nind_bygen[i]),function(i){sample(c(1,2),1)})))
+      colnames(Aux)<-c("index","father","mother","sex")
+      deb<-rbind(deb,Aux)
+
+      m=m+nind_bygen[i]
+      
+    }
+    } else{
+      for(i in 1: ngen){
+        
+        Aux=data.frame(seq(m+1,m+nind_bygen[i]),sample(x=unlist(deb[deb[,4]==1,1]),size=nind_bygen[i],replace=TRUE),
+                       sample(unlist(deb[deb[,4]==2,1]),nind_bygen[i],replace=TRUE),
+                       unlist(lapply(c(1:nind_bygen[i]),function(i){sample(c(1,2),1)})))
+        colnames(Aux)<-c("index","father","mother","sex")
+        deb<-rbind(deb,Aux)
+        
+        m=m+nind_bygen[i]
+        
+      }     
+      }
+    pedigree<-deb
+    colnames(pedigree)<-c()
+  }  
+A<-list("pedigree"=pedigree,"founder"=nfounder,"generations"=ngen)
+
+if(!is.null(path)){
+write.table(pedigree,paste0(path,"/File_st.ped"),col.names = FALSE,row.names = FALSE,
+           quote=FALSE)
+}else{
+write.table(pedigree,"File_st.ped",sep="\t",col.names = FALSE, row.names=FALSE, 
+            quote=FALSE)
+  
+}
+  return(A)          
+
+}
+
+pedgenerator(100,4,c(rep(100,3),150),sex=FALSE,path="~/Dropbox/DoctoradoCRAG/paper-1/article/software-help/reversinn1/pSBVB/toy_strawberry/Additional_functions"
+,exclude=47)
